9e90d74e9897cd98b552b45fd2bb0682e6a6d811
KHALID ELSEADAWY <95033222+kseadawy@users.noreply.github.com>
2023-12-05 21:56:17 +0300

    Adding code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
+//Node Class
+class TrieNode{
+  
+  constructor(){
+    this.children = {};
+    this.eow = false;//means end of word
+  }
+}
 
+//This class is for trie data structure which is works well with spell checker or autocomplete
+class Trie{
+  
+  constructor(){
+    this.root = new TrieNode();
+  }
+
+  insert(word){
+    let curNode = this.root;
+    
+    for(let c of word){
+      if(!curNode.children[c])
+          curNode.children[c] = new TrieNode();
+      curNode = curNode.children[c];
+    }
+    curNode.eow = true;
+  }
+
+  search(word){
+    let curNode = this.root;
+
+    for(let c of word){
+      if(!curNode.children[c])
+          return false;
+      curNode = curNode.children[c];
+    }
+    return curNode.eow;
+  }
+
+  startsWith(word){
+    let curNode = this.root;
+
+    for(let c of word){
+      if(!curNode.children[c])
+          return false;
+      curNode = curNode.children[c];
+    }
+    return true;
+  }
+}
+
+
+let tree = new Trie();
+tree.insert("apple"); console.log("Inserted: apple ");
+console.log("Searched: apple " + tree.search("apple"));
+console.log("Searched: app " + tree.search("app"));
+console.log("started with : app " + tree.startsWith("app"));
